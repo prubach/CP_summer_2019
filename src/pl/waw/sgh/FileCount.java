22528b709833f977b2db9133e802ff3d014b9bd7
prubac <prubac@sgh.waw.pl>
2019-04-04 12:44:42 +0200

    Counting size of folders recursively

--- src/pl/waw/sgh/FileCount.java
+++ src/pl/waw/sgh/FileCount.java
@@ -4,15 +4,26 @@ import java.io.File;
 
 public class FileCount {
 
+    public static long sumSize(File inFile) {
+        long sum = 0;
+        if (inFile.isDirectory()) {
+            for (File f : inFile.listFiles()) {
+                System.out.println(f.toString() + " " + f.length());
+                //sum += f.length();
+                sum += sumSize(f);
+            }
+        } else {
+            return inFile.length();
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         String INPUT_PATH = "c:\\temp";
         File mypath = new File(INPUT_PATH);
 
-        if (mypath.isDirectory()) {
-            for (File f : mypath.listFiles()) {
-                System.out.println(f.toString() + " " + f.length());
-            }
-        }
+        System.out.println(sumSize(mypath));
+
 
     }
 }
