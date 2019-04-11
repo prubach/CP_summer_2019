8f3c6e87cd942fa611589f2c63a615c10dda63eb
prubac <prubac@sgh.waw.pl>
2019-04-11 11:22:25 +0200

    Rectangle class - constructors

--- src/pl/waw/sgh/obj/Rectangle.java
+++ src/pl/waw/sgh/obj/Rectangle.java
@@ -2,8 +2,23 @@ package pl.waw.sgh.obj;
 
 public class Rectangle {
 
-    double parA;
+    Double parA;
     double parB;
+
+    public Rectangle() {
+        parA = 5.0;
+        parB = 6.0;
+    }
+
+    public Rectangle(double parA, double parB) {
+        setParams(parA, parB);
+    }
+
+    public Rectangle(double parA) {
+        this(parA, 0.0);
+        //super(); // Call constructor of super class (parent)
+        //this.parA = parA;
+    }
 
     public void setParams(double parA, double b) {
         this.parA = parA;
